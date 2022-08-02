ec0a4e6db608630f56ef262fab07f8c46f405748
ruukichi <ngovietlc93@gmail.com>
2022-08-02 18:37:29 +0700

    vietnq thay doi bien

--- src/com/codegym/StudentManagement.java
+++ src/com/codegym/StudentManagement.java
@@ -6,28 +6,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class StudentManagement {
-    private static List<Student> students = new ArrayList<>();
+    private static List<Student> studentArrayList = new ArrayList<>();
 
 
     public static void displayListStudent() {
-        for (Student student : students) {
+        for (Student student : studentArrayList) {
             System.out.println(student);
         }
     }
 
     public static void addStudent(Student student) {
-        students.add(student);
+        studentArrayList.add(student);
     }
 
     public int size() {
-        return students.size();
+        return studentArrayList.size();
     }
 
     public static Student findStudentByName(String name) {
         Student student = null;
-        for (int index = 0; index < students.size(); index++) {
-            if (students.get(index).getName().equals(name)){
-                student = students.get(index);
+        for (int index = 0; index < studentArrayList.size(); index++) {
+            if (studentArrayList.get(index).getName().equals(name)){
+                student = studentArrayList.get(index);
             }
         }
         return student;
